627efe51c64bd85d44e8371c5611972601cbb911
Martin Neundorfer <m.neundorfer@labor.digital>
2020-05-06 20:11:41 +0200

    feat: merge in php70

--- php70/php70/opt/bootstrap.sh
+++ php70/php70/opt/bootstrap.sh
@@ -6,7 +6,6 @@ export APACHE_LOG_DIR=${APACHE_LOG_DIR:-"/var/www/logs"}
 export PROJECT_ENV=${PROJECT_ENV:-"prod"}
 source /opt/directories.sh
 set_permissions
-source /opt/bootstrap-dev.sh
 source /opt/bootstrap-project.sh
 set_permissions
 
