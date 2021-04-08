bec0dd8e37778dc1a75e0e415214d5f41963d4f0
Martin Neundorfer <m.neundorfer@labor.digital>
2021-04-08 17:13:51 +0200

    feat: add a dummy server for node images

--- node12/node12/opt/index.js
+++ node12/node12/opt/index.js
@@ -3,3 +3,19 @@ console.log("ARGS: ", process.argv);
 setInterval(() => {
 	console.log("Node app is still running! Waiting 10 seconds...");
 }, 10000);
+
+const http = require('http');
+
+const port = '8000';
+
+const app = new http.Server();
+
+app.on('request', (req, res) => {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.write('Hello World');
+	res.end('\n');
+});
+
+app.listen(port, () => {
+	console.log(`Node app is listening on port ${port}`);
+});
